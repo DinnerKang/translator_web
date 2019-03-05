f24458e575aafd1917b31a14605650927913fea1
DinnerKang <jenhyuk0318@gmail.com>
2019-03-05 16:46:38 +0900

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,80 @@
+var database = firebase.database();
+
+var data_length;
+var korean = [];
+var english = [];
+var time_table = [];
+
+var cutWords = ['퇴직'];
+
+function writeUserData() {
+    return new Promise(function (resolve, reject) {
+        firebase
+            .database()
+            .ref("translate/success")
+            .on("value", function (snapshot) {
+                console.log(snapshot.val());
+                var data = snapshot.val();
+                var len = document.getElementById("data_length");
+                var top10 = document.getElementById("top10_list");
+                var _values = Object.values(data);
+
+                data_length = Object.keys(data).length;
+                len.innerText = data_length;
+
+                for (var i = 0; i < data_length; i++) {
+                    korean[i] = _values[i].Korean;
+                    english[i] = _values[i].English;
+                    time_table[i] = _values[i].Time;
+                }
+
+                korean = makeResult(korean.reduce(reducer, []));
+                english = makeResult(english.reduce(reducer, {}));
+                console.log(korean);
+                /*
+                var top_sorting = korean.sort(function (a, b) {
+                    return b.freq < a.freq ? -1 : b.freq > a.freq ? 1 : 0;
+                }).slice(0, 50);
+                console.log(top_sorting);*/
+                resolve(korean);
+            });
+    });
+}
+var reducer = function (acc, val, idx, arr) {
+    if (acc.hasOwnProperty(val)) {
+        acc[val] = acc[val] + 1;
+    } else {
+        acc[val] = 1;
+    }
+    return acc;
+};
+
+function makeResult(obj) {
+    var arr = [];
+    var key = Object.keys(obj);
+    var freq = Object.values(obj);
+    for (var i = 0; i < key.length; i++) {
+        arr.push([key[i], freq[i]]);
+    }
+
+    return arr;
+}
+function array_diff(a, b) {
+    var tmp={}, res=[];
+    for(var i=0;i<a.length;i++) tmp[a[i]]=1;
+    for(var i=0;i<b.length;i++) { if(tmp[b[i]]) delete tmp[b[i]]; }
+    for(var k in tmp) res.push(k);
+    return res;
+  }
+
+
+writeUserData().then(
+    res => {
+        console.log(res);
+        var option = {
+            list: res,
+            weightFactor: 20
+        }
+        WordCloud(document.getElementById('my_canvas'), option);
+    }
+);
